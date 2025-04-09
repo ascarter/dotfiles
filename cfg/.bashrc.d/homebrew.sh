2990b6876829f2ee83b45aa43cfb21e89404b873
Andrew Carter <ascarter@users.noreply.github.com>
2025-04-09 11:31:56 -0700

    Add 1Password and Homebrew shell configuration

---
+++ cfg/.bashrc.d/homebrew.sh
@@ -0,0 +1,7 @@
+# Homebrew configuration
+export HOMEBREW_NO_EMOJI=1
+if [ -d /opt/homebrew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -d /home/linuxbrew/.linuxbrew ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
