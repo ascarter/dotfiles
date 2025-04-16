0030cafdaa2010075078a52d2a898174644406b4
Andrew Carter <ascarter@users.noreply.github.com>
2025-04-15 22:11:25 -0700

    Check if docker is installed for completions

--- etc/profile.d/docker.sh
+++ etc/profile.d/docker.sh
@@ -1,6 +1,8 @@
 # Shell completions for docker
-if [ -n "$BASH_VERSION" ]; then
-  source <(docker completion bash)
-elif [ -n "$ZSH_VERSION" ]; then
-  source <(docker completion zsh)
+if command -v docker > /dev/null 2>&1 ; then
+	if [ -n "$BASH_VERSION" ]; then
+	  source <(docker completion bash)
+	elif [ -n "$ZSH_VERSION" ]; then
+	  source <(docker completion zsh)
+	fi
 fi
