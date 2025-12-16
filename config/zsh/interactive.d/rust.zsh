95939f5d872e4a97e0ec90825eae9bf2177f83a9
Andrew Carter <ascarter@users.noreply.github.com>
2025-12-16 13:34:02 -0800

    Disable color output from cargo in zsh config

--- config/zsh/interactive.d/rust.zsh
+++ config/zsh/interactive.d/rust.zsh
@@ -1,6 +1,9 @@
 export RUSTUP_HOME="${XDG_DATA_HOME}"/rustup
 export CARGO_HOME="${XDG_DATA_HOME}"/cargo
 
+# Disable color output from cargo
+export CARGO_TERM_COLOR=never
+
 # Rust shell configuration
 if [[ -d ${CARGO_HOME} ]]; then
   . "${CARGO_HOME}"/env
