78d87a1c3a31f585e3b7b41d66129216de2f1724
Andrew Carter <ascarter@users.noreply.github.com>
2025-12-25 15:47:58 -0800

    Load fnm shell integration only if default Node.js is installed

--- config/zsh/interactive.d/fnm.zsh
+++ config/zsh/interactive.d/fnm.zsh
@@ -2,6 +2,10 @@ export FNM_DIR="${FNM_DIR:-${XDG_DATA_HOME}/fnm}"
 
 # Add fnm to PATH and configure shell integration
 if (( $+commands[fnm] )); then
-  eval "$(fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled --shell zsh)"
   eval "$(fnm completions --shell zsh)"
+
+  # Ensure fnm has default Node.js installed before enabling shell integration.
+  if fnm list | grep -q 'default'; then
+    eval "$(fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled --shell zsh)"
+  fi
 fi
