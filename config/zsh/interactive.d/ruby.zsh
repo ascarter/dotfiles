1a968a476f1ba492f022c7ac10a5c7956bdde6d8
Andrew Carter <ascarter@users.noreply.github.com>
2026-01-04 14:09:07 -0800

    Remove legacy rbenv and Ruby config from zsh setup

--- config/zsh/interactive.d/ruby.zsh
+++ config/zsh/interactive.d/ruby.zsh
@@ -1,11 +1,3 @@
-export RBENV_ROOT="${XDG_DATA_HOME}"/rbenv
-export RUBY_CONFIGURE_OPTS="--enable-yjit"
-export RV_COLOR=never
-
-# Ruby shell configuration
 if (( $+commands[rv] )); then
   eval "$(rv shell init zsh)"
-  eval "$(rv shell completions zsh)"
-elif (( $+commands[rbenv] )); then
-  eval "$(rbenv init -)"
 fi
