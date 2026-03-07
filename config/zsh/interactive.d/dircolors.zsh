3c6e50d3c9325b50a5ee6d2c0ebbfb27854db48c
Andrew Carter <ascarter@users.noreply.github.com>
2026-03-06 22:33:44 -0800

    Support uu-dircolors as alternative to dircolors

--- config/zsh/interactive.d/dircolors.zsh
+++ config/zsh/interactive.d/dircolors.zsh
@@ -1,3 +1,7 @@
-if (( $+commands[dircolors] )) && [[ -f ${ZDOTDIR}/dircolors ]]; then
-  eval "$(dircolors ${ZDOTDIR}/dircolors)"
+if [[ -f ${ZDOTDIR}/dircolors ]]; then
+  if (( $+commands[uu-dircolors] )); then
+    eval "$(uu-dircolors ${ZDOTDIR}/dircolors)"
+  elif (( $+commands[dircolors] )); then
+    eval "$(dircolors ${ZDOTDIR}/dircolors)"
+  fi
 fi
