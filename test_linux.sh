a248087f0d76993e903b99e4528bef741fc32746
Andrew Carter <ascarter@users.noreply.github.com>
2026-04-07 12:29:46 -0700

    Clean up scripts and update plugin versions
    - Remove obsolete migration and developer scripts
    - Rename and reorganize scripts for clarity
    - Drop eza from aqua packages
    - Update Neovim plugin lockfile revisions
    - Simplify script runner in bin/dotfiles

--- test_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-image="fedora-dotfiles"
-volumes="-v $PWD/.:/home/dev/.local/share/dotfiles"
-docker build -t ${image} .
-docker run -it --rm ${volumes} --name fedora-dotfiles ${image}:latest bash
