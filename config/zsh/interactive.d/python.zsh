dc534ffef6004dcb9da389fcdbbab158ff229a6f
Andrew Carter <ascarter@users.noreply.github.com>
2026-05-15 11:12:37 -0700

    Export UV_PYTHON_BIN_DIR and update PATH for uv

--- config/zsh/interactive.d/python.zsh
+++ config/zsh/interactive.d/python.zsh
@@ -1,4 +1,7 @@
-UV_NO_MODIFY_PATH=1
+export UV_NO_MODIFY_PATH=1
+export UV_PYTHON_BIN_DIR="$XDG_DATA_HOME/uv/bin"
+
+path=(${UV_PYTHON_BIN_DIR} $path)
 
 if (( $+commands[uv] )); then
   if [[ -o interactive ]]; then
