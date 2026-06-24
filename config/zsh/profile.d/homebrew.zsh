d16f478a773a88c823a6ac6862d7ff8808986305
Andrew Carter <ascarter@users.noreply.github.com>
2026-06-24 12:38:40 -0700

    Disable Homebrew cask auto-update upgrades

--- config/zsh/profile.d/homebrew.zsh
+++ config/zsh/profile.d/homebrew.zsh
@@ -4,6 +4,9 @@ export HOMEBREW_NO_EMOJI=1
 export HOMEBREW_DOWNLOAD_CONCURRENCY=auto
 export HOMEBREW_NO_ENV_HINTS=1
 
+# Prefer app-native updaters for self-updating Homebrew casks.
+export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
+
 if [[ -d /opt/homebrew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ -d /home/linuxbrew/.linuxbrew ]]; then
