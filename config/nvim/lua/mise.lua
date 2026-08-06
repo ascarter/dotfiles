7b47ae20c2df50c8d3bda7de8ebb68c51c760f21
Andrew Carter <ascarter@users.noreply.github.com>
2026-08-05 17:23:03 -0700

    Load mise shim PATH in Neovim startup

---
+++ config/nvim/lua/mise.lua
@@ -0,0 +1,6 @@
+-- =============================================================================
+-- mise configuration
+-- =============================================================================
+
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
