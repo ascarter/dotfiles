96b800688496f108d0b843f4ec41e73a108145cf
Andrew Carter <ascarter@users.noreply.github.com>
2026-08-05 23:29:13 -0700

    Move dotfiles to ~/.dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 set -eu
 
 readonly DOTFILES_REPO_URL="https://github.com/ascarter/dotfiles.git"
-readonly DOTFILES_HOME="${HOME}/Developer/dotfiles"
+readonly DOTFILES_HOME="${HOME}/.dotfiles"
 readonly MISE_BIN="${HOME}/.local/bin/mise"
 readonly MISE_CONFIG="${DOTFILES_HOME}/mise.toml"
 
